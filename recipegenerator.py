2b36f554b32b0e922c9b49427950960943be3949
lnasrall23 <122539627+lnasrall23@users.noreply.github.com>
2023-04-24 17:59:25 -0400

    Update recipegenerator.py
    1. Added a new line asking users preferred meal type depending on time of the day

--- recipegenerator.py
+++ recipegenerator.py
@@ -4,6 +4,7 @@ import pandas as pd
 recipe_data = pd.read_csv('recipes.csv')
 
 # Ask the user for their dietary preferences, ingredient restrictions, and preferred cuisine
+time_of_day= input('Insert type of meal (e.g. breakfast, lunch, dinner, snack):')
 dietary_preferences = input('What are your dietary preferences? (e.g. vegetarian, gluten-free): ')
 ingredient_restrictions = input('Do you have any ingredient restrictions? (e.g. nuts, dairy): ')
 preferred_cuisine = input('What is your preferred cuisine? (e.g. Italian, Chinese): ')
@@ -13,7 +14,6 @@ filtered_recipes = recipe_data[
     recipe_data['dietary_preferences'].str.contains(dietary_preferences, na=False) &
     ~recipe_data['ingredients'].str.contains(ingredient_restrictions, na=False) &
     recipe_data['cuisine'].str.contains(preferred_cuisine, na=False)
-]
 
 # Display the suggested recipes to the user
 if len(filtered_recipes) == 0:
