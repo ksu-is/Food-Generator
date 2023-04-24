ae018e024d8cd4924246b7785b40b9fe0c955096
lnasrall23 <122539627+lnasrall23@users.noreply.github.com>
2023-04-24 18:18:06 -0400

    Update recipegenerator.py

--- recipegenerator.py
+++ recipegenerator.py
@@ -9,15 +9,18 @@ dietary_preferences = input('What are your dietary preferences? (e.g. vegetarian
 ingredient_restrictions = input('Do you have any ingredient restrictions? (e.g. nuts, dairy): ')
 preferred_cuisine = input('What is your preferred cuisine? (e.g. Italian, Chinese): ')
 
+
 # Filter the recipe data based on the user's preferences
 filtered_recipes = recipe_data[
     recipe_data['dietary_preferences'].str.contains(dietary_preferences, na=False) &
     ~recipe_data['ingredients'].str.contains(ingredient_restrictions, na=False) &
     recipe_data['cuisine'].str.contains(preferred_cuisine, na=False)
-
+]
 # Display the suggested recipes to the user
 if len(filtered_recipes) == 0:
     print('Sorry, no recipes found that match your preferences.')
+    if len(filtered_recipes)==7:
+        print('Here are your suggested seven recipes.')
 else:
     print(f'Suggested recipes based on your preferences ({len(filtered_recipes)} found):')
     for index, row in filtered_recipes.iterrows():
